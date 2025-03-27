857c591902a10e54907ad2994525a243fe4e9f13
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-03-27 17:19:27 -0300

    feat: Criação do Decorator.ts para validação de compra e venda

---
+++ src/types/Decorators.ts
@@ -0,0 +1,24 @@
+export function ValidaCompra(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function(valorCompra: number) {
+        if(valorCompra <= 0)
+            throw new Error("O valor da compra precisa ser maior do que zero!");
+
+        if(valorCompra > this.saldo)
+            throw new Error("Seu saldo é insuficiente para realizar a operação!");
+
+        return originalMethod.apply(this, [valorCompra]) // Chama o método original
+    }
+}
+
+export function ValidaVenda(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function(valorVenda: number) {
+        if(valorVenda <= 0)
+            throw new Error("O valor da Venda precisa ser maior do que zero!");
+
+        return originalMethod.apply(this, [valorVenda]) // Chama o método original
+    }
+}
