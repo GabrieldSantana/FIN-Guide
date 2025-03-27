79dd72bdf18219b2cb02459b3ba06b12383a2af8
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-03-27 17:17:34 -0300

    feat: Criação do extrato-component.ts par adicionar dados a tabela

---
+++ src/components/extrato-component.ts
@@ -0,0 +1,41 @@
+import conta from "../types/Conta";
+import { Transacao } from "../types/Transacao";
+import { formatarMoeda } from "../utils/formatters";
+
+// Elemento Transação Extrato
+const elemTransacaoExt: HTMLElement = document.querySelector(".extrato");
+
+renderizarExtrato();
+
+function renderizarExtrato(): void {
+    const transacoes: Transacao[] = conta.getTransacoes();
+    elemTransacaoExt.innerHTML = "";
+
+    let i = 1;
+    let htmlRegistrosTransacoes: string = "";
+
+    for(let transacao of transacoes) {
+        htmlRegistrosTransacoes += `
+        <tr>
+            <th scope="row"> ${i} </th>
+            <td><h5 class="txt-roxo-claro"> ${transacao.nomeProduto} </h5></td>
+            <td> ${transacao.quantidade} </td>
+            <td><h5 class="txt-roxo-claro"> ${formatarMoeda(transacao.valor)} </h5></td>
+            <td><button class="btn text-bg-dark"><i class="bi bi-trash3"></i></button></td>
+        </tr>
+        `;
+        i++
+    }
+
+    if(htmlRegistrosTransacoes == "") htmlRegistrosTransacoes = "<div>Não há transações registradas!</div>";
+
+    elemTransacaoExt.innerHTML = htmlRegistrosTransacoes;
+}
+
+// const ExtratoComponent = {
+//     atualizar(): void {
+//         renderizarExtrado()
+//     }
+// }
+
+// export default ExtratoComponent;
