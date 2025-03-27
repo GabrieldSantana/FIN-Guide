a4eb7940f31341a15abcf00526e913ae774dca62
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-03-27 17:18:14 -0300

    fix: alteração no método estático "obter"

--- src/types/Armazenador.ts
+++ src/types/Armazenador.ts
@@ -6,12 +6,10 @@ export class Armazenador {
         localStorage.setItem(chave, valorComoString);
     }
 
-    static obter<T>(chave: string, reviver?: (this: any, key: string, value: any) => any): T | null {
+    static obter<T>(chave: string): T | null {
         const valor = localStorage.getItem(chave);
 
         if(valor === null) return null;
-
-        if(reviver) return JSON.parse(valor, reviver) as T
 
         return JSON.parse(valor) as T;
     }
