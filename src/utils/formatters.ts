af6c32ff6a2d211d8c05924e6cec9c3defd302c6
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-03-28 17:49:21 -0300

    feat: adição de validação na função formatarMoeda

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,4 +1,7 @@
 // Exportando a função que recebe um número e devolve uma string
-export function formatarMoeda(valor: number) : string {
-    return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+export function formatarMoeda(valor: number | undefined): string {
+    return (valor ?? 0).toLocaleString("pt-br", { 
+        style: "currency", 
+        currency: "BRL" 
+    });
 }
