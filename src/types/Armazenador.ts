6b7c2194f37d8de26cdbf228651d1fb5b5c28066
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-03-31 17:45:36 -0300

    feat: Criação de métodos para remover registro

--- src/types/Armazenador.ts
+++ src/types/Armazenador.ts
@@ -1,16 +1,27 @@
+import conta from "./Conta";
+
 export class Armazenador {
     private constructor() {} //Não permite estanciar a classe forçando a utilização dos métodos estáticos
 
-    static salvar<T>(chave: string, valor: any): void {
-        const valorComoString = JSON.stringify(valor);
-        localStorage.setItem(chave, valorComoString);
+    static salvar(chave: string, valor: any): void {
+        const valorComoString = JSON.stringify(valor); // COnverte o valor para string
+        localStorage.setItem(chave, valorComoString); // Armazena no localStorage
     }
 
     static obter<T>(chave: string): T | null {
-        const valor = localStorage.getItem(chave);
+        const valor = localStorage.getItem(chave); // Recupera o valor do localStorage
 
         if(valor === null) return null;
 
         return JSON.parse(valor) as T;
+    }
+
+    static apagarRegistros(chave: string): void {
+        localStorage.removeItem(chave)
+    }
+    
+    static remover(index: number): any {
+        const transacoes = conta.getTransacoes();
+        return transacoes.splice(index, 1);
     }
 }
