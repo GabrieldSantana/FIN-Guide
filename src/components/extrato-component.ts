b58b36b6e2b202cbbf7e34c2328d520e99f0dcaa
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-03-31 17:40:56 -0300

    fix: Correção da função renderizarExtrato

--- src/components/extrato-component.ts
+++ src/components/extrato-component.ts
@@ -1,9 +1,10 @@
 import conta from "../types/Conta.js";
+import { TipoTransacao } from "../types/TipoTransacao.js";
 import { Transacao } from "../types/Transacao.js";
 import { formatarMoeda } from "../utils/formatters.js";
 
 // Elemento Transação Extrato
-const elemTransacaoExt: HTMLElement = document.getElementById("sessao-extrato");
+const elemTransacaoExt: HTMLElement = document.getElementById("extrato");
 
 renderizarExtrato();
 
@@ -11,30 +12,50 @@ function renderizarExtrato(): void {
     const transacoes: Transacao[] = conta.getTransacoes();
     elemTransacaoExt.innerHTML = "";
 
-    let i = 1;
-    let htmlRegistrosTransacoes: string = "";
+    console.log(transacoes);
 
-    for(let transacao of transacoes) {
+    let htmlRegistrosTransacoes: string = "";
+    let total = 0;
+
+    for (const transacao of transacoes) {
+        const sinal = transacao.tipoTransacao === TipoTransacao.COMPRA ? '-' : '+';
+        const classeSinal = transacao.tipoTransacao === TipoTransacao.COMPRA ? 'text-danger' : 'text-success';
+        
+        // Calcula o valor total da transação (quantidade * valor unitário)
+        const valorTransacao = (transacao.quantidade) * (transacao.valor);
+
+        const multiplicador = (transacao.tipoTransacao === TipoTransacao.COMPRA ? -1 : 1);
+        total += multiplicador * valorTransacao;
+
         htmlRegistrosTransacoes += `
-        <tr>
-            <th scope="row"> ${i} </th>
-            <td><h5 class="txt-roxo-claro"> ${transacao.nomeProduto} </h5></td>
-            <td> ${transacao.quantidade} </td>
-            <td><h5 class="txt-roxo-claro"> ${formatarMoeda(transacao.valor)} </h5></td>
-            <td><button class="btn text-bg-dark"><i class="bi bi-trash3"></i></button></td>
-        </tr>
+            <tr class="table-row">
+                <th class="${classeSinal}">${sinal}</th>
+                <td>${transacao.nomeProduto}</td>
+                <td>${transacao.quantidade}</td>
+                <td>${formatarMoeda(transacao.valor)}</td>
+                <td><button id="btn-excluir" class="btn-excluir btn text-bg-dark"><i class="bi bi-trash3"></i></button></td>
+            </tr>
         `;
-        i++
     }
 
-    // if(htmlRegistrosTransacoes == "") htmlRegistrosTransacoes = "<div>Não há transações registradas!</div>";
 
-    // elemTransacaoExt.innerHTML = htmlRegistrosTransacoes;
+    // Adiciona a linha de total
+    htmlRegistrosTransacoes += `
+        <tr>
+            <th scope="row"></th>
+            <td><h5 class="txt-roxo-claro">Total</h5></td>
+            <td></td>
+            <td><h5 id="valor-total" class="txt-roxo-claro">${formatarMoeda(total)}</h5></td>
+            <td></td>
+        </tr>
+    `;
+
+    elemTransacaoExt.innerHTML = htmlRegistrosTransacoes;
 }
 
 const ExtratoComponent = {
     atualizar(): void {
-        this.renderizarExtrato()
+        renderizarExtrato()
     }
 }
 
