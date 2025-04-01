080c4bc09b346ac9de7f409aa82099859815dd7a
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-01 14:31:45 -0300

    fix: Correção da função alternarDisplay

--- src/utils/trocarDisplay.ts
+++ src/utils/trocarDisplay.ts
@@ -1,13 +1,10 @@
-// export function alternarDisplay(elemento: HTMLElement): void {
-//     if (elemento.style.display === "none") {
-//         elemento.style.display = "block";
-//     } else {
-//         elemento.style.display = "none";
-//     }
-// }
-
-
-export function alternarDisplay(elemento: HTMLElement): void {
-    elemento.classList.toggle('d-none');
-    elemento.classList.toggle('d-block');
+// trocarDisplay.ts
+export function alternarDisplay(elemento: HTMLElement, visivel?: boolean): void {
+    if (visivel !== undefined) {
+        elemento.classList.toggle("d-none", !visivel);
+        elemento.classList.toggle("d-block", visivel);
+    } else {
+        elemento.classList.toggle("d-none");
+        elemento.classList.toggle("d-block");
+    }
 }
