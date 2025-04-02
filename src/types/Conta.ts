67ba4d0c6ff1c464e5c746c935b632ef813ab554
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-02 19:26:01 -0300

    feat: Adição de comentários

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -4,7 +4,7 @@ import { TipoTransacao } from "./TipoTransacao.js";
 import { Transacao } from "./Transacao.js";
 
 export class Conta {
-    protected saldo: number = Armazenador.obter<number>("saldo") ?? 2000;
+    protected saldo: number = Armazenador.obter<number>("saldo") ?? 2000; // definindo o saldo de acordo com o que está armazenado no localStorage
     private transacoes: Transacao[] = Armazenador.obter<Transacao[]>("transacoes") ?? [];
 
     constructor() {}
@@ -17,9 +17,13 @@ export class Conta {
         return this.transacoes;
     }
 
+
+    // Método para registrar transacao, recebendo um objeto Transacao
     registrarTransacao(novaTransacao: Transacao): void {
+
+        // Verificando se o tipo da transacao é compra ou venda
         if(novaTransacao.tipoTransacao == TipoTransacao.COMPRA) {
-            this.comprar(novaTransacao.valor * novaTransacao.quantidade);
+            this.comprar(novaTransacao.valor * novaTransacao.quantidade); // Realizando a compra de acordo com a quantidade
             console.log("comprado")
         }
         else if(novaTransacao.tipoTransacao == TipoTransacao.VENDA) {
@@ -27,19 +31,19 @@ export class Conta {
             console.log("vendido")
         }
 
-        this.transacoes.push(novaTransacao);
-        Armazenador.salvar("transacoes", this.transacoes);
+        this.transacoes.push(novaTransacao); // Armazenando a transacao na lista de transações
+        Armazenador.salvar("transacoes", this.transacoes); // Salvando a nova transacao no localStorage
     }
 
-    @ValidaCompra
+    @ValidaCompra //Decorator de validacao
     comprar(valor:number): boolean {
         try {
-            this.saldo -= valor;
-            Armazenador.salvar("saldo", this.saldo);
+            this.saldo -= valor; //subtraindo o valor da compra do saldo
+            Armazenador.salvar("saldo", this.saldo); //Armazenando o novo saldo no localStorage
         }
         catch (error) {
         alert(error.message);
-    }
+        }
         return true;
     }
 
@@ -51,5 +55,5 @@ export class Conta {
     }
 }
 
-const conta = new Conta();
-export default conta;
+const conta = new Conta(); // Estanciando uma nova conta
+export default conta; // Exportando a conta para utilizar seus métodos
