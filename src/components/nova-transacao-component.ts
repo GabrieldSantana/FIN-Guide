0f1831f8019a7a6f1e589a1bd799cc1ce68e0e06
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-02 19:12:37 -0300

    fix: alteração do tipo do valor e realocação do evento de mudança de página

--- src/components/nova-transacao-component.ts
+++ src/components/nova-transacao-component.ts
@@ -1,9 +1,10 @@
 import Conta from "../types/Conta.js";
 import { TipoTransacao } from "../types/TipoTransacao.js";
 import { Transacao } from "../types/Transacao.js";
-import { alternarDisplay } from "../utils/trocarDisplay.js";
+import { ConverteValorTransacao, ValidaNomeProduto, ValidaQuantidadeProduto } from "../utils/validators.js";
 import ExtratoComponent from "./extrato-component.js";
 import SaldoComponent from "./saldo-component.js";
+
 
 const elementoForm = document.querySelector(".form form") as HTMLFormElement;
 
@@ -26,20 +27,26 @@ elementoForm.addEventListener("submit", function(event) {
         let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
         let nomeProduto: string = inputNomeProduto.value;
         let quantidade: number = inputQuantidade.valueAsNumber;
-        let valor: number = inputValor.valueAsNumber;
+        let valor: string = inputValor.value; // Armazenado como string para validações
 
-        const novaTransacao: Transacao = {
-            tipoTransacao: tipoTransacao,
-            nomeProduto: nomeProduto,
-            quantidade: quantidade,
-            valor: valor
+        // Estanciando uma nova transação para ser usada dentro da condicional
+        let novaTransacao: Transacao
+
+        let valorConvertido = ConverteValorTransacao(valor) //Convertendo o valor em float
+
+
+        // Atribuindo os valores do objeto novaTransacao caso o seu nome e sua quantidade seja válida
+        if (ValidaNomeProduto(nomeProduto) && ValidaQuantidadeProduto(quantidade)) {
+            novaTransacao = {
+                tipoTransacao: tipoTransacao,
+                nomeProduto: nomeProduto,
+                quantidade: quantidade,
+                valor: valorConvertido
+            }
         }
-
-        // if (!validaTransacao(novaTransacao)) {
-        //     throw new Error("Erro: Preencha todos os campos corretamente (nome, quantidade e valor)!");
-        // }
-
-        console.log(novaTransacao)
+        else {
+            throw new Error("Valor inserido inválido!")
+        }
 
         Conta.registrarTransacao(novaTransacao); //Registrando a nova transação na conta
         SaldoComponent.atualizar(); //Atualizando o saldo
@@ -51,28 +58,5 @@ elementoForm.addEventListener("submit", function(event) {
     }
 });
 
-const elementoVisualizar = document.getElementById("visualizar") as HTMLButtonElement;
-const elementoTransacao = document.getElementById("sessao-transacao") as HTMLElement;
-const elementoExtrato = document.getElementById("sessao-extrato") as HTMLElement;
 
-elementoVisualizar.addEventListener("click", function(event) {
-    try {
-        event.preventDefault(); // Para não recarregar a página
 
-        console.log("Visualizar clicado");
-
-        // Verifica o estado atual (se transação está visível)
-        const transacaoVisivel = !elementoTransacao.classList.contains("d-none");
-
-        // Alterna as seções: mostra uma, esconde a outra
-        alternarDisplay(elementoTransacao, !transacaoVisivel);
-        alternarDisplay(elementoExtrato, transacaoVisivel);
-
-        // // Ajusta o texto do botão conforme a seção visível
-        // elementoVisualizar.textContent = transacaoVisivel ? "Nova Transação" : "Visualizar Extrato";
-
-        alternarDisplay(elementoTransacao);
-        alternarDisplay(elementoExtrato);
-
-    } catch (error) { error.message }
-});
