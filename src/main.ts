5c61dbb0b3e74039592229025edc1b19b834a7cb
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-02 17:28:33 -0300

    feat: adição de imports na main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import "./components/nova-transacao-component.js";
 import "./components/saldo-component.js";
 import "./components/extrato-component.js";
+import "./utils/mascara.js";
+import "./utils/informacoesModal.js"; 
+import "./utils/trocarDisplay.js"
